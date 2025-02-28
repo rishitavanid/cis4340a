5d87d40155a716e4e0bd101dbc9952862bf05125
rishitavanid <ri102003@ucf.edu>
2025-02-28 17:24:53 -0500

    Update R09_LCK08_J.java

--- R09_LCK08_J.java
+++ R09_LCK08_J.java
@@ -3,20 +3,55 @@
  * LCK10-J. Use a correct form of the double-checked locking idiom
  * The double-checked locking idiom is a software design pattern used to reduce the overhead of acquiring a lock by first testing the locking criterion 
  * without actually acquiring the lock. 
+ * 
+ * Non-Compliant Code 
+ * public final class Client {
+ * private final Lock lock = new ReentrantLock();
+ * public void doSomething(File file) {
+ *  InputStream in = null;
+ *   try {
+ *    in = new FileInputStream(file);
+ *     lock.lock();
+ *     // Perform operations on the open file
+ *   } catch (FileNotFoundException fnf) {
+ *     // Forward to handler
+ *   } finally {
+ *     lock.unlock();
+ *     if (in != null) {
+ *       try {
+ *         in.close();
+ *       } catch (IOException e) {
+ *         // Forward to handler
+ *       }
+ *     }
+ *   }
+ * }
+ * }
+ * 
  ******************************************************************************/
-// Works with acquire/release semantics for volatile
-// Broken under JDK 1.4 and earlier
-final class Foo {
-  private volatile Helper helper = null;
+
+// Compliant Code 
+public final class Client {
+  private final Lock lock = new ReentrantLock();
  
-  public Helper getHelper() {
-    if (helper == null) {
-      synchronized (this) {
-        if (helper == null) {
-          helper = new Helper();
+  public void doSomething(File file) {
+    InputStream in = null;
+    lock.lock();
+    try {
+      in = new FileInputStream(file);
+      // Perform operations on the open file
+    } catch (FileNotFoundException fnf) {
+      // Forward to handler
+    } finally {
+      lock.unlock();
+ 
+      if (in != null) {
+        try {
+          in.close();
+        } catch (IOException e) {
+          // Forward to handler
         }
       }
     }
-    return helper;
   }
 }

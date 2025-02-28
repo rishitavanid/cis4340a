e95d5c2d0b4907a2defb5dc39323f4acc33ae20c
rishitavanid <ri102003@ucf.edu>
2025-02-28 16:57:53 -0500

    Update R01_DCL00_J.java

--- R01_DCL00_J.java
+++ R01_DCL00_J.java
@@ -3,17 +3,31 @@
  * DCL00-J. Prevent class initialization cycles
  * Initialization of a class consists of executing its static initializers and the initializers for static fields (class variables) declared in the class.
  *
+ * Non-Compliant Code 
+ * 
+ * public class Cycle {
+ * private final int balance;
+ * private static final Cycle c = new Cycle();
+ * private static final int deposit = (int) (Math.random() * 100); // Random deposit
+ * public Cycle() {
+ * balance = deposit - 10; // Subtract processing fee
+ * }
+ * public static void main(String[] args) {
+ * System.out.println("The account balance is: " + c.balance);
+ * }
+ * }
+ * 
  ******************************************************************************/
 
+// Compliant Code 
 public class Cycle {
   private final int balance;
-  private static final Cycle c = new Cycle();
   private static final int deposit = (int) (Math.random() * 100); // Random deposit
-
+  private static final Cycle c = new Cycle();  // Inserted after initialization of required fields
   public Cycle() {
     balance = deposit - 10; // Subtract processing fee
   }
-
+ 
   public static void main(String[] args) {
     System.out.println("The account balance is: " + c.balance);
   }

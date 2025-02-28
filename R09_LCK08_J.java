eca3e793427830fdea6643be9b5bc0745a5e5371
rishitavanid <ri102003@ucf.edu>
2025-02-28 17:43:42 -0500

    Add files via upload

--- R09_LCK08_J.java
+++ R09_LCK08_J.java
@@ -3,20 +3,32 @@
  * LCK10-J. Use a correct form of the double-checked locking idiom
  * The double-checked locking idiom is a software design pattern used to reduce the overhead of acquiring a lock by first testing the locking criterion 
  * without actually acquiring the lock. 
- ******************************************************************************/
-// Works with acquire/release semantics for volatile
-// Broken under JDK 1.4 and earlier
-final class Foo {
-  private volatile Helper helper = null;
  
-  public Helper getHelper() {
-    if (helper == null) {
-      synchronized (this) {
-        if (helper == null) {
-          helper = new Helper();
-        }
+ ******************************************************************************/
+
+public final class Client {
+  private final Lock lock = new ReentrantLock();
+ 
+  public void doSomething(File file) {
+    InputStream in = null;
+    try {
+      in = new FileInputStream(file);
+      lock.lock();
+ 
+      // Perform operations on the open file
+ 
+      lock.unlock();
+    } catch (FileNotFoundException x) {
+      // Handle exception
+    } finally {
+      if (in != null) {
+        try {
+          in.close();
+        } catch (IOException x) {
+          // Handle exception
+        } 
       }
     }
-    return helper;
   }
 }
+

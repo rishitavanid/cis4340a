b26dbcc9a8c5d78d91b310e720baaf0ba060d00c
rishitavanid <ri102003@ucf.edu>
2025-02-28 16:50:04 -0500

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -2,18 +2,38 @@
  * Rule 06. Methods (MET)
  * MET01-J. Never use assertions to validate method arguments
  *
+ * Non-Compliant Code 
+ * public static int getAbsAdd(int x, int y) {
+ * assert x != Integer.MIN_VALUE;
+ * assert y != Integer.MIN_VALUE;
+ * int absX = Math.abs(x);
+ * int absY = Math.abs(y);
+ * assert (absX <= Integer.MAX_VALUE - absY);
+ * return absX + absY;
+ * }
+ * public static void main(String[] args) {
+ * System.out.println(getAbsAdd(Integer.MIN_VALUE, 1));
+ * }
+ * }
+ *
  ******************************************************************************/
 
-public static int getAbsAdd(int x, int y) {
-  assert x != Integer.MIN_VALUE;
-  assert y != Integer.MIN_VALUE;
-  int absX = Math.abs(x);
-  int absY = Math.abs(y);
-  assert (absX <= Integer.MAX_VALUE - absY);
-  return absX + absY;
-}
+// Compliant Code 
 
-public static void main(String[] args) {
-  System.out.println(getAbsAdd(Integer.MIN_VALUE, 1));
-}
+public class R06_MET01_J {
+    public static int getAbsAdd(int x, int y) {
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException();
+        }
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+        if (absX > Integer.MAX_VALUE - absY) {
+            throw new IllegalArgumentException();
+        }
+        return absX + absY;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getAbsAdd(Integer.MIN_VALUE, 1));
+    }
 }

a40b89392f52712214db45101ece9cc2e184b707
rishitavanid <ri102003@ucf.edu>
2025-02-28 16:08:23 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,11 +2,24 @@
  * Rule 02. Expressions (EXP)
  * XP00-J. Do Not Ignore Values Returned By Methods
  *
+ * Non-Compliant Code 
+ * 
+ * public void deleteFile() {
+ * File someFile = new File("someFileName.txt");
+ * // Do something with someFile
+ * someFile.delete();
+ * }
+ * 
  ******************************************************************************/
 
+// Compliant Code 
+
 public void deleteFile() {
-  File someFile = new File("someFileName.txt");
-  // Do something with someFile
-  someFile.delete();
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+    if (!someFile.delete()) {
+        // Handle failure to delete the file
+    }
 }
+
 

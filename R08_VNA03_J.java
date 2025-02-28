43a1d6cb9270c1bf7b35a2db589b818eb10951ae
rishitavanid <ri102003@ucf.edu>
2025-02-28 17:18:01 -0500

    Update R08_VNA03_J.java

--- R08_VNA03_J.java
+++ R08_VNA03_J.java
@@ -4,6 +4,24 @@
  * When presented with a set of operations, where each is guaranteed to be atomic, it is tempting to assume that a single operation consisting of individually atomic 
  * operations is guaranteed to be collectively atomic without additional locking.
  * 
+ * Non-Compliant Code 
+ * 
+ * final class Adder {
+ * private final AtomicReference<BigInteger> first;
+ * private final AtomicReference<BigInteger> second;
+ * public Adder(BigInteger f, BigInteger s) {
+ *   first  = new AtomicReference<BigInteger>(f);
+ *   second = new AtomicReference<BigInteger>(s);
+ * }
+ * public void update(BigInteger f, BigInteger s) { // Unsafe
+ *   first.set(f);
+ *   second.set(s);
+ * }
+ * public BigInteger add() { // Unsafe
+ *   return first.get().add(second.get());
+ * }
+ * }
+ * 
  ******************************************************************************/
 final class Adder {
   // ...
